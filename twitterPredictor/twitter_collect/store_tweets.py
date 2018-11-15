7dde17f2f7d90d1bd09d619bb964a32baf366a28
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-15 10:42:15 +0000

    Add files via upload
    Stocker les tweets dans des fichiers

--- twitterPredictor/twitter_collect/store_tweets.py
+++ twitterPredictor/twitter_collect/store_tweets.py
@@ -1,5 +1,22 @@
 import json
+# on a avec la fonction collect une liste de dictionnaire
+# on transforme cette liste en liste de liste
+def liste(tweet):
+    L=[]
+    for D in tweet:
+        L1=[]
+        for clé in d.keys():
+            L1.append("'clé' : D[clé]")
+        L.append(L1)
+    return L
+
 
 def store_tweets(tweets,filename):
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(playlist, f, indent=4)
+    # tweets est une liste de liste
+    with open('filename'.json, 'w', encoding='utf-8') as f:
+        tweet_json=json.dump(tweets, f, indent=4)
+    print tweet_json
+
+
+
+
