a0fc01bf154f27b93fccaa13fb294b504137ee19
ulrichYepmou <45000634+ulrichYepmou@users.noreply.github.com>
2018-11-15 22:36:47 +0100

    Add files via upload

---
+++ test_nomdumoduleatester.py
@@ -0,0 +1,17 @@
+from twitter_predictor.twitter_collect import collect
+from pytest import *
+
+
+def test_collect():
+    tweets = tweet_collect.collect()
+    data =  transform_to_dataframe(tweets)
+    assert 'tweet_textual_content' in data.columns
+    
+def test_collect2():
+    import twitterPredictor.py import *
+    tweets = tweet_collect.collect()
+    data =  transform_to_dataframe(tweets)
+    assert 'tweet_textual_content' in data.columns
+    
+
+    
